57b27799acf3cf6930a6690889f637e3c2c9efb7
Rahul Jadhav <nyrahul@gmail.com>
2018-02-21 19:04:24 +0530

    added npdao vs dco data

---
+++ data/plotter.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#num  tot_nodes  lf_nodes  unconn_nodes  stale_entries  tot_par_sw  elap_time  dao_sent  dao_rcvd  npdao_sent  npdao_rcvd  dco_sent  dco_rcvd  udp_sent  udp_rcvd
+#1    100        49        15            77             48          120        294       1674      0           0           40        165       155       152
+
+function finish()
+{
+    [[ -f "$TMP_F" ]] && rm $TMP_F
+    echo "bye"
+}
+trap finish EXIT
+
+COL_ELAPTIME=7
+COL_DCO_RCVD=13
+COL_NPDAO_RCVD=11
+COL_STALE_ENT=5
+
+DATADIR="data_n100_udp30"
+DCO_F=$DATADIR/dco_0.csv
+NPDAO_F=$DATADIR/npdao_0.csv
+
+TMP_F=tmp_$$.tmp
+
+common_plot_lines()
+{
+    echo "
+    set datafile separator ','
+    set key autotitle columnhead
+    set key left
+    set xlabel 'Time(sec)'
+    set style line 1 lw 2
+    set style line 2 lw 2
+    set term png
+    "
+}
+
+plot_ctrl_overhead()
+{
+    echo "
+    `common_plot_lines`
+    set title 'DCO vs NPDAO ctrl overhead' font ',15'
+    set ylabel '#msgs'
+    set output '$DATADIR/dco_vs_npdao_ctrl_overhead.png'
+
+    plot '$NPDAO_F' using $COL_ELAPTIME:$COL_NPDAO_RCVD ls 1 title 'cumulative npdao' with lines, \
+         '$DCO_F' using $COL_ELAPTIME:$COL_DCO_RCVD ls 2 title 'cumulative dco' with lines
+    " | gnuplot
+    echo "Plotted ctrl overhead"
+}
+
+plot_stale_stats()
+{
+    echo "
+    `common_plot_lines`
+    set title 'DCO vs NPDAO stale entries stats'
+    set xlabel 'Time(sec)'
+    set ylabel 'Stale Routing Entries'
+    set output '$DATADIR/dco_vs_npdao_stale_stats.png'
+
+    plot '$NPDAO_F' using $COL_ELAPTIME:$COL_STALE_ENT title 'with NPDAO' with lines, \
+         '$DCO_F' using $COL_ELAPTIME:$COL_STALE_ENT title 'with DCO' with lines
+    " | gnuplot
+    echo "Plotted stale stats"
+}
+
+plot_ctrl_overhead
+plot_stale_stats
+
